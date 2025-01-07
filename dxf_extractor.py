0f928aa9280e0731497f2a52d42a6867e2002d6f
Ritik Shah <vulture1302@gmail.com>
2025-01-07 11:46:13 +0530

    new regex for inventory item and new rule addition

--- dxf_extractor.py
+++ dxf_extractor.py
@@ -10,8 +10,9 @@ class DXFExtractor:
     def __init__(self,doc,density,img_doc) -> None:
         self.parts_regex_pattern = r"\\A1;\d+X\d+X\d+ \w+(\~\d+)?"
         self.phase_regex_pattern=r"~PHASE_\d+/\d+"
-        self.pipe_regex_MTEXT_pattern=r'\\A\d+;\d+ PB\d~{\\A\d+;\\C\d+;\d+NB \(M\)PIPE}'
-        self.pipe_regex_DIMENSION_pattern=r'<> PB\d~{\\A\d+;\\C\d+;\d+NB \(M\)PIPE}'
+        self.inventory_item_regex=r"^\d+ [A-Za-z0-9]+_[A-Za-z0-9()]+(~\d+)?$"
+        # self.pipe_regex_MTEXT_pattern=r'\\A\d+;\d+ PB\d~{\\A\d+;\\C\d+;\d+NB \(M\)PIPE}'
+        # self.pipe_regex_DIMENSION_pattern=r'<> PB\d~{\\A\d+;\\C\d+;\d+NB \(M\)PIPE}'
         self.doc=doc
         self.density=density
         self.logger=logging.getLogger(self.__class__.__name__)
@@ -107,33 +108,71 @@ class DXFExtractor:
                                     except Exception as e:
                                         self.logger.error(f"Error  {e}")
                                         
-                            elif virtual_entity.dxftype()=="MTEXT" and re.match(self.pipe_regex_MTEXT_pattern,virtual_entity.dxf.text):
-                                
+                            elif virtual_entity.dxftype()=="MTEXT" and re.match(self.inventory_item_regex,virtual_entity.dxf.text):
                                 try:
-                                    part_str=virtual_entity.dxf.text[4:]
-                                    length,str0,pipename=part_str.split(" ")
-                                    partname,str1=str0.split('~')
-                                    pipe_name=str1.split(';')[2]
-                                    pipe_mark=pipe_name+pipename[0:3]
-                                    pipe=next((item for item in inventory_list if item["itemDescription"] == pipe_mark), None)
-                                    area=(2*math.pi*math.pow(int(pipe["thickness"])/2,2)+math.pi*int(pipe["thickness"])*int(length))/1000000
-                                    volume=(math.pi*math.pow(int(pipe["thickness"])/2,2)*int(length))/1000000000
-                                    weight=int(pipe["weightPerMeter"])*int(length)/1000
+                                    part_str=virtual_entity.dxf.text.strip()
+                                    length,name=part_str.split(" ")
+                                    part_mark,inventory_item=name.split("_")
+                                    if '~' in inventory_item:
+                                        inventory_part_name,qty=inventory_item.split('~')
+                                    else :
+                                        qty=1
+                                        inventory_part_name=inventory_item
                                     
-                                    if partname not in duplicate_check_dict[block.name]:
+                                    inventory_part_details=next( (item for item in inventory_list if item["itemDescription"] == inventory_part_name), None)
+                                    if inventory_part_details is None:
+                                        self.logger.error(f"Error  No such inventory item exists")
+                                        continue
+                                    
+                                    weight=int(inventory_part_details["weightPerMeter"])*int( length)/1000
+                                    
+                                    if part_mark not in duplicate_check_dict[block.name]:
                                         block_wise_parts_dict[block.name]['parts'].append({
-                                        "Part Name": partname.upper()+" "+f"({pipe_mark} PIPE)",
-                                        "Thickness (mm)": int(pipe["thickness"]),
-                                        "Quantity": 1,
-                                        "Length (mm)": int(length),
-                                        "Width (mm)": int(pipe["thickness"]),
-                                        "Area (m2)": round(area,2) if round(area,2)!=0 else area,
-                                        "Volume (m3)": round(volume,2) if round(volume,2)!=0 else volume,
-                                        "Weight (kg)": round(weight,2) if round(weight,2)!=0 else weight 
+                                            "Part Name":part_mark.upper(),
+                                            "Thickness (mm)": int(inventory_part_details["thickness"]),
+                                            "Quantity": int(qty),
+                                            "Length (mm)": int(length),
+                                            "Width (mm)":0,
+                                            "Area (m2)":0,
+                                            "Volume (m3)":0,
+                                            "Weight (kg)": round(weight, 2) if round(weight, 2)!=0 else weight
                                         })
-                                        duplicate_check_dict[block.name][partname]=True
-                                except Exception as e:
-                                    self.logger.error(f"Error  yoo  {e}")
+                                        duplicate_check_dict[block.name][part_mark]=True
+                                    
+                                        
+                                    
+                                except Exception as e :
+                                    self.logger.error(f"Error  {e}")
+                                
+                            
+                                        
+                            # elif virtual_entity.dxftype()=="MTEXT" and re.match(self.pipe_regex_MTEXT_pattern,virtual_entity.dxf.text):
+                                
+                            #     try:
+                            #         part_str=virtual_entity.dxf.text[4:]
+                            #         length,str0,pipename=part_str.split(" ")
+                            #         partname,str1=str0.split('~')
+                            #         pipe_name=str1.split(';')[2]
+                            #         pipe_mark=pipe_name+pipename[0:3]
+                            #         pipe=next((item for item in inventory_list if item["itemDescription"] == pipe_mark), None)
+                            #         area=(2*math.pi*math.pow(int(pipe["thickness"])/2,2)+math.pi*int(pipe["thickness"])*int(length))/1000000
+                            #         volume=(math.pi*math.pow(int(pipe["thickness"])/2,2)*int(length))/1000000000
+                            #         weight=int(pipe["weightPerMeter"])*int(length)/1000
+                                    
+                            #         if partname not in duplicate_check_dict[block.name]:
+                            #             block_wise_parts_dict[block.name]['parts'].append({
+                            #             "Part Name": partname.upper()+" "+f"({pipe_mark} PIPE)",
+                            #             "Thickness (mm)": int(pipe["thickness"]),
+                            #             "Quantity": 1,
+                            #             "Length (mm)": int(length),
+                            #             "Width (mm)": int(pipe["thickness"]),
+                            #             "Area (m2)": round(area,2) if round(area,2)!=0 else area,
+                            #             "Volume (m3)": round(volume,2) if round(volume,2)!=0 else volume,
+                            #             "Weight (kg)": round(weight,2) if round(weight,2)!=0 else weight 
+                            #             })
+                            #             duplicate_check_dict[block.name][partname]=True
+                            #     except Exception as e:
+                            #         self.logger.error(f"Error  yoo  {e}")
                                     
                     elif entity.dxftype() == "MTEXT" and re.match(self.phase_regex_pattern, entity.dxf.text):
                        try:
